c5ebf89062d45bb23994606ae94455fda7a98621
KBerry4492 <kmb23@comcast.net>
2017-12-07 16:27:18 -0500

    workingatlast v1.0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ScrapeMon
+
+ScrapeMon pulls articles from Kotaku.com and adds them to a database, as well as allowing them to be displayed and commented on.
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/week18Populater', {
+mongoose.connect('mongodb://localhost/scrapeMondb', {
   useMongoClient: true
 });
 
@@ -42,30 +42,40 @@ app.get('/scrape', function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
     // Now, we grab every h2 within an article tag, and do the following:
-    $('article h2').each(function(i, element) {
+    $('article').each(function(i, element) {
       // Save an empty result object
       var result = {};
 
+      console.log("PING")
+
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children('a')
+        .children('header').children('h1').children('a')
         .text();
       result.link = $(this)
-        .children('a')
+        .children('header').children('h1').children('a')
         .attr('href');
+      result.summ = $(this)
+        .children('div.item__content').children('div.excerpt').children('p')
+        .text();
+
+      
+      console.log("-------");
+      console.log(result);
+      console.log("-------");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
           // If we were able to successfully scrape and save an Article, send a message to the client
-          console.log('scrape complete', dbArticle);
+          console.log('Scraped!', dbArticle);
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
           res.json(err);
         });
     });
-    res.send('Scrape Complete');
+    res.send('Articles Scraped');
   });
 });
 
@@ -83,12 +93,12 @@ app.get('/articles', function(req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
+// Route for grabbing a specific Article by id, populate it with it's Comment
 app.get('/articles/:id', function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate('note')
+    // ..and populate all of the Comments associated with it
+    .populate('Comment')
     .then(function(dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
@@ -99,15 +109,15 @@ app.get('/articles/:id', function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
+// Route for saving/updating an Article's associated Comment
 app.post('/articles/:id', function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+  // Create a new Comment and pass the req.body to the entry
+  db.Comment.create(req.body)
+    .then(function(dbComment) {
+      // If a Comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
